281176e129a1eee6ceb06cd81169f10ddc119eee
Tastan Kulmeshkenov <58576437+taskul@users.noreply.github.com>
2023-07-30 19:44:36 -0500

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ const PORT = +process.env.PORT || 3001;
 function getDatabaseUri() {
   return (process.env.NODE_ENV === "postgresql:///test")
       ? "jobly_test"
-      : process.env.DATABASE_URL || "postgresql:///jobly";
+      : process.env.DATABASE_URL || "postgresql:///jobly_ho1s";
 }
 
 // Speed up bcrypt during tests, since the algorithm safety isn't being tested
